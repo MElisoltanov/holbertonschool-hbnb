61f93f5b96e92a2827a0ec3b1a6c7d9562d0debb
ofest <dars20@hotmail.com>
2025-10-13 14:35:09 +0200

    Update place with the conditions of the attributes.

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -4,14 +4,25 @@ from app.models.BaseModel import BaseModel
 class Place(BaseModel):
     def __init__(self, title, description, price, latitude, longitude, owner):
         super().__init__()
+        if not title or len(title) > 100:
+            raise ValueError("Title is required max 100 chars.")
+        if price <= 0:
+            raise ValueError("Price must be positive.")
+        if not (-90 <= latitude <= 90):
+            raise ValueError("Latitude must be between -90 and 90.")
+        if not (-180 <= longitude <= 180):
+            raise ValueError("Longitude must be between -180 and 180.")
+        if not owner:
+            raise ValueError("Place must have an owner User instance.")
+        
         self.title = title
         self.description = description
         self.price = price
         self.latitude = latitude
         self.longitude = longitude
         self.owner = owner
-        self.reviews = []  # List to store related reviews
-        self.amenities = []  # List to store related amenities
+        self.reviews = []
+        self.amenities = []
 
     def add_review(self, review):
         """Add a review to the place."""
