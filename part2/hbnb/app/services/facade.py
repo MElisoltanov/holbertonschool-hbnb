dbde749414b0d29eabbc56792a6b35bf5a245622
flor4 <salanson.flora@outlook.fr>
2025-10-15 09:59:38 +0200

    add: amenities part to the facade

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -16,3 +16,23 @@ class HBnBFacade:
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(self, **amenity_data):
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+        
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+        for key, value in amenity_data.items():
+            setattr(amenity, key, value)
+        self.amenity_repo.update(amenity)
+        return amenity
