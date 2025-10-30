a9988edf8360428493049c6efd3d6dad500180f7
MElisoltanov <214495859+MElisoltanov@users.noreply.github.com>
2025-10-31 00:23:01 +0100

    Update config file

--- part2/hbnb/config.py
+++ part2/hbnb/config.py
@@ -3,9 +3,14 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
+    
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'super-secret-jwt-key')
+    JWT_ACCESS_TOKEN_EXPIRES = 3600
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
     'development': DevelopmentConfig,
