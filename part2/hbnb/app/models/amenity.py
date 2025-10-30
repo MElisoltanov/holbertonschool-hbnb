e09ea3e620a2bc665dd53749f38d397a22aec910
MElisoltanov <214495859+MElisoltanov@users.noreply.github.com>
2025-10-31 00:39:03 +0100

    Update amenity file

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,21 +1,22 @@
 from app.models.BaseModel import BaseModel
+from app.Extensions import db
 
 
 class Amenity(BaseModel):
+    __tablename__ = 'amenities'
+
+    name = db.Column(db.String(50), nullable=False, unique=True)
 
     def __init__(self, name):
-
         super().__init__()
         self.set_name(name)
 
     def set_name(self, name):
-
         if not name:
             raise ValueError("Amenity name is required.")
         if len(name) > 50:
             raise ValueError("Amenity name must be 50 characters or fewer.")
         self.name = name
-        self.save()  # Update updated_at timestamp from BaseModel
 
     def to_dict(self):
         return {
@@ -24,5 +25,4 @@ class Amenity(BaseModel):
         }
 
     def __str__(self):
-
-        return "Amenity(id={}, name={})".format(self.id, self.name)
+        return f"Amenity(id={self.id}, name={self.name})"
