74516e439d72a1f0e9704469e345582cf3c549a0
ofest <dars20@hotmail.com>
2025-11-04 09:39:53 +0100

    Update.

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,14 +1,25 @@
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
+from config import DevelopmentConfig
+
 from app.api.v1.places import api as places_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
 
+db = SQLAlchemy()  # initialize db instance globally
 
-def create_app():
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
+    app.config.from_object(config_class)
+    # Initialize SQLAlchemy with app
+    db.init_app(app)
+
+    # Set up API
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+
+    # Register namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(places_ns, path="/api/v1/places")
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
