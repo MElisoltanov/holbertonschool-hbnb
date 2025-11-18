879d7880b56050d54bf0afc6e39dcf4d8eee40e3
MElisoltanov <214495859+MElisoltanov@users.noreply.github.com>
2025-11-18 13:25:23 +0100

    Add feature for login

--- part4/scripts.js
+++ part4/scripts.js
@@ -1,10 +1,5 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
+
     fetch('header.html')
       .then(response => response.text())
       .then(html => {
@@ -16,4 +11,34 @@ document.addEventListener('DOMContentLoaded', () => {
       .then(html => {
         document.getElementById('footer').innerHTML = html;
       });
+
+      const loginForm = document.getElementById('login-form');
+      if (loginForm) {
+        loginForm.addEventListener('submit', async (event) => {
+          event.preventDefault();
+
+          const email = document.getElementById('email').value;
+          const password = document.getElementById('password').value;
+
+          try {
+            const response = await fetch('http://localhost:5000/api/v1/auth/login', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ email, password })
+            });
+
+            if (response.ok) {
+              const data = await response.json();
+              document.cookie = `token=${data.access_token}; path=/`;
+              window.location.href = 'index.html';
+            } else {
+             alert('Login failed : Check your informations.');
+            }
+          } catch (error) {
+           alert('Network or server error. Please try again later.');
+           }
+        });
+      }
   });
